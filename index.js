13ca315775c009d061263cfb1bd262abb9011a2e
CountBleck <Mr.YouKnowWhoIAm@protonmail.com>
2022-12-23 11:55:29 -0800

    Append a __lunaticSerialize method to every class
    This code has been *yoinked* from ASON's transform to an extent. There
    are plenty of changes in syntax, but the same core logic is being used.

--- index.js
+++ index.js
@@ -1,10 +1,140 @@
-import {ElementKind} from "assemblyscript"
+import {CommonFlags, Node, NodeKind, ParameterKind, ElementKind} from "assemblyscript"
 import {Transform} from "assemblyscript/transform"
 
 const INTERNAL_TRANSFORM_NAME = "LunaticInternalTransformInterface"
 const METHOD_NAMES = ["__lunaticSerialize"]
 
 export default class LunaticTransform extends Transform {
+    afterParse({sources}) {
+        for (const {statements} of sources) {
+            this.traverseStatements(statements)
+        }
+    }
+
+    traverseStatements(statements) {
+        for (const statement of statements) {
+            if (statement.kind === NodeKind.ClassDeclaration) {
+                this.createSerializeMethod(statement)
+            } else if (statement.kind === NodeKind.NamespaceDeclaration) {
+                this.traverseStatements(statement.members)
+            }
+        }
+    }
+
+    createSerializeMethod({members, name: {range}, isGeneric}) {
+        const bodyStatements = members
+            .filter(member => member.is(CommonFlags.Instance) && member.kind === NodeKind.FieldDeclaration)
+            .map(({name}) => {
+                const {range, text} = name
+                // ser.write(this.PROP, offsetof<this>("PROP"));
+                return Node.createExpressionStatement(
+                    Node.createCallExpression(
+                        Node.createPropertyAccessExpression(
+                            Node.createIdentifierExpression("ser", range),
+                            Node.createIdentifierExpression("write", range),
+                            range
+                        ),
+                        null,
+                        [
+                            Node.createPropertyAccessExpression(
+                                Node.createThisExpression(range),
+                                name,
+                                range
+                            ),
+                            Node.createCallExpression(
+                                Node.createIdentifierExpression("offsetof", range),
+                                [
+                                    Node.createNamedType(
+                                        Node.createSimpleTypeName("this", range),
+                                        null,
+                                        false,
+                                        range
+                                    )
+                                ],
+                                [Node.createStringLiteralExpression(text, range)],
+                                range
+                            )
+                        ],
+                        range
+                    )
+                )
+            })
+
+        // if (isDefined(super.__lunaticSerialize)) super.__lunaticSerialize(ser);
+        const superFunction = Node.createPropertyAccessExpression(
+            Node.createSuperExpression(range),
+            Node.createIdentifierExpression("__lunaticSerialize", range),
+            range
+        )
+        const ser = Node.createIdentifierExpression("ser", range)
+
+        bodyStatements.push(
+            Node.createIfStatement(
+                Node.createCallExpression(
+                    Node.createIdentifierExpression("isDefined", range),
+                    null,
+                    [superFunction],
+                    range
+                ),
+                Node.createExpressionStatement(
+                    Node.createCallExpression(
+                        superFunction,
+                        null,
+                        [ser],
+                        range
+                    )
+                ),
+                null,
+                range
+            )
+        )
+
+        // __lunaticSerialize<T>(ser: T): void { ... }
+        const flags = CommonFlags.Public | CommonFlags.Instance | CommonFlags.Generic | (isGeneric && CommonFlags.GenericContext)
+        members.push(
+            Node.createMethodDeclaration(
+                Node.createIdentifierExpression("__lunaticSerialize", range),
+                null,
+                flags,
+                [
+                    Node.createTypeParameter(
+                        Node.createIdentifierExpression("T", range),
+                        null,
+                        null,
+                        range
+                    ),
+                ],
+                Node.createFunctionType(
+                    [
+                        Node.createParameter(
+                            ParameterKind.Default,
+                            ser,
+                            Node.createNamedType(
+                                Node.createSimpleTypeName("T", range),
+                                null,
+                                false,
+                                range
+                            ),
+                            null,
+                            range
+                        )
+                    ],
+                    Node.createNamedType(
+                        Node.createSimpleTypeName("void", range),
+                        null,
+                        false,
+                        range
+                    ),
+                    null,
+                    false,
+                    range
+                ),
+                Node.createBlockStatement(bodyStatements, range),
+                range
+            )
+        )
+    }
+
     afterInitialize(program) {
         const classes = [...program.elementsByName.values()]
             .filter(element => {
